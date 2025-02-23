c7de478005e4d39af8e4a5a8c1c3ff36c433203c
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-02-23 13:09:19 -0300

    Resolução da lista 2 de LP IV
    Resolução do exercício 5

--- Linguage de Progrmação IV/lista_exercicio_2/exercicio5.php
+++ Linguage de Progrmação IV/lista_exercicio_2/exercicio5.php
@@ -4,24 +4,29 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 4</title>
+    <title>Exercício 5</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Divisão</h1>
+    <h1>Média escolar</h1>
 
-    <form method="post" action="exercicio4Resposta.php">
+    <form method="post" action="exercicio5Resposta.php">
 
         <div class="mb-3">
-            <label for="valor1" class="form-label">Informe o primeiro valor</label>
-            <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            <label for="nota1" class="form-label">Informe o primeiro nota</label>
+            <input type="number" id="nota1" name="nota1" class="form-control" required="">
         </div>
 
         <div class="mb-3">
-            <label for="valor2" class="form-label">Informe o segundo valor</label>
-            <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            <label for="nota2" class="form-label">Informe o segundo nota</label>
+            <input type="number" id="nota2" name="nota2" class="form-control" required="">
+        </div>
+
+        <div class="mb-3">
+            <label for="nota3" class="form-label">Informe o terceira nota</label>
+            <input type="number" id="nota3" name="nota3" class="form-control" required="">
         </div>
 
         <button type="submit" class="btn btn-primary">Enviar</button>
