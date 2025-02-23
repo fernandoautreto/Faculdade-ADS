d8aad7acc88fb098991e369168d41f5baf42787a
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-02-23 17:15:32 -0300

    resolução da lista 2 de LP IV
    resolução do exercício 18 e pequenas correções do exercício 17

--- Linguage de Progrmação IV/lista_exercicio_2/exercicio18.php
+++ Linguage de Progrmação IV/lista_exercicio_2/exercicio18.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 17</title>
+    <title>Exercício 18</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <form method="post" action="exercicio17resposta.php">
+    <form method="post" action="exercicio18resposta.php">
 
         <div class="mb-3">
             <label for="capital" class="form-label">Informe o capital: </label>
--- Linguage de Progrmação IV/lista_exercicio_2/exercicio18resposta.php
+++ Linguage de Progrmação IV/lista_exercicio_2/exercicio18resposta.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 17</title>
+    <title>Exercício 18</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -16,7 +16,7 @@
             $capital = str_replace(',', '.', $_POST['capital']);
             $periodo = str_replace(',', '.', $_POST['periodo']);
             $juros = str_replace(',', '.', $_POST['juros']);
-            $capJuros = $capital * ($periodo/100) * $juros;
+            $capJuros = $capital * ((1+($juros/100)) ** $periodo);
             echo "<p>Capital: R$ $capital</p><p> Taxa de juros: $juros %</p> <p>Período: $periodo</p> <p>Juros: R$ $capJuros</p>";
         } catch (Exception $e) {
             echo $e->getMessage();
