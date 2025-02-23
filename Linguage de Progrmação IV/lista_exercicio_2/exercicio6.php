5d2d891585e9590a8bb70c94e9932a5a3aae7301
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-02-23 13:19:21 -0300

    resolução da lista 2 de LP IV
    resolução do exercício 6.

---
+++ Linguage de Progrmação IV/lista_exercicio_2/exercicio6.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Conversor de °C para °F</h1>
+
+    <form method="post" action="exercicio6Resposta.php">
+
+        <div class="mb-3">
+            <label for="tempC" class="form-label">Informe a temperatura em Graus Celsius (°C)</label>
+            <input type="number" id="temoC" name="tempC" class="form-control" required="">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
