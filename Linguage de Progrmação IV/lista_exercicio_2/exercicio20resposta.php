5fc9a4e77835b153941f87ffb02a1f73d8625199
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-02-23 17:43:13 -0300

    resolução da lista 2 de LP IV
    resolução dos exercícios 19 e 20.

---
+++ Linguage de Progrmação IV/lista_exercicio_2/exercicio20resposta.php
@@ -0,0 +1,31 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 20</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            //(floatval)tranforma um str em numero|(str_replace)vai fazer que mude a , para . se for necessário
+            $distancia = str_replace(',', '.', $_POST['distancia']);
+            $tempo = str_replace(',', '.', $_POST['tempo']);
+            $velocidade = $distancia / $tempo;
+            echo "A velocidade média é de: $velocidade m/s";
+            
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
