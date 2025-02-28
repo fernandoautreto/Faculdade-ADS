f6baf48c91d57ca0d7bdfe2276d81e8bbc4eb182
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-02-28 15:21:31 -0300

    resolução da lista 3
    resolução do ex 03

---
+++ Linguage de Progrmação IV/lista_exercicio_3/ex03resp.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 01</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $n1 = $_POST["n1"];
+            $n2 = $_POST["n2"];
+            if ($n1 < $n2) {
+                echo "N1:$n1 , N2:$n2";
+            }
+            elseif ($n1 > $n2){
+                echo "N2:$n2 , N1:$n1";
+            }
+            else
+                echo "Os dois número são iguai: $n1";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
