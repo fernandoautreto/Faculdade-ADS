c9dc2dbfebbe3214edc0f2f60c33284c0212f68d
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-03-01 11:56:00 -0300

    resolução da lista 3
    resolução do exercício 4.

---
+++ Linguage de Progrmação IV/lista_exercicio_3/ex04.php
@@ -0,0 +1,29 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    
+    <form method="post" action="ex04resp.php">
+
+              
+        <div class="mb-3">
+            <label for="price" class="form-label">Informe um valor: </label>
+            <input type="text" id="price" name="price" class="form-control" step="0,01" required="">
+        </div>  
+        
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Linguage de Progrmação IV/lista_exercicio_3/ex04resp.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 04</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {
+            $preco = str_replace(',', '.', $_POST['price']);
+            if ($preco > 100) {
+                $desconto = $preco - ($preco * 0.15);
+                echo "O preço com desconto de 15%: R$ $desconto";
+            }
+            else{
+                echo "Preço cheio: R$ $preco";
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
