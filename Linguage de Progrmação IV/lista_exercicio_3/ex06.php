f74db595fb0aaac9b906b25f72a616ffe48f4f37
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-03-01 12:29:27 -0300

    resolução da lista 03
    resolução do exercício 06;

--- Linguage de Progrmação IV/lista_exercicio_3/ex06.php
+++ Linguage de Progrmação IV/lista_exercicio_3/ex06.php
@@ -3,36 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 5</title>
+    <title>Exercício 6</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div>
-        <h6>Escolha um mês, e digite o número correspondente:</h6>
-        <ol>
-            <li>Janeiro</li>
-            <li>Fevereiro</li>
-            <li>Março</li>
-            <li>Abril</li>
-            <li>Maio</li>
-            <li>Junho</li>
-            <li>Julho</li>
-            <li>Agosto</li>
-            <li>Setembro</li>
-            <li>Outubro</li>
-            <li>Novembro</li>
-            <li>Dezembro</li>
-        </ol>
-    </div>
+       <form method="post" action="ex06resp.php">
 
-    <form method="post" action="ex05resp.php">
-
-            
         <div class="mb-3">
-            <label for="mes" class="form-label">Informe um valor: </label>
-            <input type="numb" id="mes" name="mes" class="form-control" required="">
+            <label for="numero" class="form-label">Informe um valor: </label>
+            <input type="numb" id="numero" name="numero" class="form-control" required="">
         </div>  
         
         <button type="submit" class="btn btn-primary">Enviar</button>
