e43f233b2dea03306b733663a8c69d8bfd39d5ac
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-03-09 15:09:32 -0300

    resolução da lista 4
    resolução do exercício 6.

---
+++ Linguage de Progrmação IV/lista_exercicio_4/ex06resp.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1); //obriga a tipagem da qualquer método ou atributo
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 5</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+    function arredondaarNumero(float $numero): float{
+        return round($numero,0);
+    }
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        try {
+            $numero = floatval($_POST['numero']);
+            echo arredondaarNumero($numero);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
