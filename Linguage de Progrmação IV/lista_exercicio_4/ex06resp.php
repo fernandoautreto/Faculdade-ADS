c63cfb139a6ebd8b3a6793c33d31ef1e26efc18f
Fernando Autreto <fernandoautretofilho@gmail.com>
2025-03-09 15:49:48 -0300

    alguns ajustes e correções.

--- Linguage de Progrmação IV/lista_exercicio_4/ex06resp.php
+++ Linguage de Progrmação IV/lista_exercicio_4/ex06resp.php
@@ -8,7 +8,7 @@ declare(strict_types=1); //obriga a tipagem da qualquer método ou atributo
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 5</title>
+    <title>Exercício 6</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
---
+++ Linguage de Progrmação IV/lista_exercicio_4/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista de Exercício 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Lista de Atividade 4</h1>
+    <ul>
+        <li><a href="ex01.php" title="Exercício 1" hreflang="pt-br;" target="_blank">Exercício 1</a></li>
+        <li><a href="ex02.php" title="Exercício 2" hreflang="pt-br;" target="_blank">Exercício 2</a></li>
+        <li><a href="ex03.php" title="Exercício 3" hreflang="pt-br;" target="_blank">Exercício 3</a></li>
+        <li><a href="ex04.php" title="Exercício 4" hreflang="pt-br;" target="_blank">Exercício 4</a></li>
+        <li><a href="ex05.php" title="Exercício 5" hreflang="pt-br;" target="_blank">Exercício 5</a></li>
+        <li><a href="ex06.php" title="Exercício 6" hreflang="pt-br;" target="_blank">Exercício 6</a></li>
+        <li><a href="ex07.php" title="Exercício 7" hreflang="pt-br;" target="_blank">Exercício 7</a></li>
+    </ul>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
